72d8619ab6f16f9632e2f378e1df38942435f7ea
AlwaysSecond <87256497+AlwaysSecond@users.noreply.github.com>
2021-07-13 17:05:16 +0000

    Deploying to gh-pages from master @ 9259d36ee69e10bafbb15f0d6fd9e00035d5bda6 🚀

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+
+// You can write your WorkAdventure script here, if any.
+// The "WA" global object is available from anywhere.
